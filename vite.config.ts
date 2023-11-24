cff87879eb6026645dc2f7e612baa423f0d3b633
jerrywu <jerrywu001@gmail.com>
2023-11-24 09:40:05 +0800

    feat: vite5

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,15 @@ import dts from 'vite-plugin-dts';
 import path from 'path';
 import Vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import { defineConfig, loadEnv } from 'vite';
+import { UserConfig, defineConfig, loadEnv } from 'vite';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import pkg from './package.json';
 
 const resolvePath = (pathName: string) => path.resolve(__dirname, pathName);
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({ mode }: UserConfig ) => {
+  const env = loadEnv(mode as string, process.cwd(), '');
 
   return {
     resolve: {
@@ -77,5 +77,5 @@ export default defineConfig(({ command, mode }) => {
       environment: 'jsdom',
       setupFiles: ['./setupTests.ts'],
     },
-  };
+  } as UserConfig;
 });
